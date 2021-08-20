b3260ec9f33a0516ee9399d21fb7817458c7f34e
Soham Dasgupta <59978919+astonishingwolf@users.noreply.github.com>
2021-08-20 19:53:33 +0530

    Update Final_control.py

--- Monte_Carlo/BlackJack/Final_control.py
+++ Monte_Carlo/BlackJack/Final_control.py
@@ -8,6 +8,15 @@ Created on Fri Aug 20 19:44:01 2021
 
 import gym
 from Control_blackjack import Agent
-
-
+import matplotlib.pyplot as plt
+if __name__ == '__main__':
+    env = gym.make('Blackjack-v0')
+    agent = Agent(eps=0.001)
+    n_episodes = 200000
+    win_lose_draw = {-1:0, 0:0 ,1:0 }
+    win_rates = []
+    for i in range(n_episodes):
+        if i>0 and i%1000 ==0:
+            pct = win_lose_draw[1]/ i
+            
 
